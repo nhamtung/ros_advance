ecb49192d395d0bee6454677f8f4da01d108c30b
nhamtung <nhamtung125@gmail.com>
2023-09-20 17:17:52 +0700

    save

--- ros_docker/check_password.sh
+++ ros_docker/check_password.sh
@@ -6,21 +6,23 @@ username="$1"
 entered_password="$2"
 arg_param="$3"
 
+check_pass_result=false
 # Hàm kiểm tra mật khẩu
 check_password() {
-    # Kiểm tra xem mật khẩu từ người dùng có khớp với mật khẩu đã lưu trữ
-    htpasswd -vb /etc/docker/htpasswd $username $entered_password
-    if [ $? -eq 0 ]; then
-      echo "Mật khẩu đúng. Truy cập được chấp nhận."
-      if [ "$arg_param" != "" ]; then
-        echo "Chay chuong trinh roslaunch"
-        source devel/setup.bash
-        roslaunch topic_pkg topic_cpp.launch
-      fi
-    else
-      echo "Mật khẩu sai. Exit terminal"
-      exit
+  # Kiểm tra xem mật khẩu từ người dùng có khớp với mật khẩu đã lưu trữ
+  htpasswd -vb /etc/docker/htpasswd $username $entered_password
+  if [ $? -eq 0 ]; then
+    echo "Mật khẩu đúng. Truy cập được chấp nhận."
+    if [ "$arg_param" != "" ]; then
+      echo "Chay chuong trinh roslaunch"
+      source devel/setup.bash
+      roslaunch topic_pkg topic_cpp.launch
     fi
+    check_pass_result=true
+  else
+    echo "Mật khẩu sai. Exit terminal"
+    check_pass_result=false
+  fi
 }
 
 if [ "$arg_param" == "" ]; then
@@ -32,4 +34,3 @@ fi
 
 # Gọi hàm kiểm tra mật khẩu
 check_password "$username" "$entered_password"
-
