a54faed85f4a1b4e3547699c391d78c321af19c4
nhamtung <nhamtung125@gmail.com>
2023-09-20 15:07:35 +0700

    save

--- ros_docker/check_password.sh
+++ ros_docker/check_password.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
 htpasswd_file="/etc/docker/htpasswd"
+
+username="$1"
+entered_password="$2"
+arg_param="$3"
+
 # Hàm kiểm tra mật khẩu
 check_password() {
-    username="$1"
-    entered_password="$2"
-    
     # Kiểm tra xem mật khẩu từ người dùng có khớp với mật khẩu đã lưu trữ
     htpasswd -vb /etc/docker/htpasswd $username $entered_password
     if [ $? -eq 0 ]; then
       echo "Mật khẩu đúng. Truy cập được chấp nhận."
-      source devel/setup.bash
-      roslaunch topic_pkg topic_cpp.launch
+      if [ "$arg_param" != "" ]; then
+        echo "Chay chuong trinh roslaunch"
+        source devel/setup.bash
+        roslaunch topic_pkg topic_cpp.launch
+      fi
     else
-      echo "Mật khẩu sai."
+      echo "Mật khẩu sai. Exit terminal"
+      exit
     fi
 }
 
-# Nhập tên người dùng và mật khẩu từ người dùng
-read -p "Nhập tên người dùng: " username
-read -s -p "Nhập mật khẩu: " entered_password
-echo
+if [ "$arg_param" == "" ]; then
+  # Nhập tên người dùng và mật khẩu từ người dùng
+  read -p "Nhập tên người dùng: " username
+  read -s -p "Nhập mật khẩu: " entered_password
+  echo
+fi
 
 # Gọi hàm kiểm tra mật khẩu
 check_password "$username" "$entered_password"
